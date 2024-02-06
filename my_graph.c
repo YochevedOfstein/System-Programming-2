9ac676722303ffc506ff8860f3fe3c6f55cb302f
Yocheved <yocheved@davidowitz.net>
2024-02-06 23:55:37 +0200

    while

--- my_graph.c
+++ my_graph.c
@@ -5,7 +5,7 @@
 
 int main(){
 
-    char func;
+    char func = 0;
 
     char A = 'A';
     char B = 'B';
@@ -14,51 +14,41 @@ int main(){
 
     int num;
 
-    int i;
-    int j;
+    int i = 0;
+    int j = 0;
 
     int graph[size][size];
 
+    while(func != D){
+        scanf("%c", &func);
 
-    scanf("%c", &func);
-
-    if(func == A){
-         for(int i=0; i<size; i++){
-            for(int j = 0; j<size; j++){
-             scanf("%d",&num);
-             graph[i][j] = num;
+        if(func == A){
+            for(int i=0; i<size; i++){
+                for(int j = 0; j<size; j++){
+                 scanf("%d",&num);
+                 graph[i][j] = num;
+                }
             }
         }
-       scanf("%c", &func); 
-    }
 
-    else if(func == B){
-       scanf("%d",&i); 
-       scanf("%d",&j);
+        else if(func == B){
+            scanf("%d",&i); 
+            scanf("%d",&j);
 
-       if(isPath(i, j, graph) == 0){
-        printf("False");
-       }
-       else{
-        printf("True");
-       }
-       scanf("%c", &func);
-    }
+            if(isPath(i, j, graph) == 0){
+                printf("False");
+            }
+            else{
+                printf("True");
+            }
+        }
 
-    else if(func == C){
-       scanf("%d",&i); 
-       scanf("%d",&j);
-       printf("%d", FloydWarshall(graph, i, j));
-       scanf("%c", &func);
+        else if(func == C){
+            scanf("%d",&i); 
+            scanf("%d",&j);
+            printf("%d", FloydWarshall(graph, i, j));
+        }
     }
-
-    else if(func == D){
-        return 0;
-    }
-    else{
-        return 1;
-    }
-
     return 0;
 }
 
